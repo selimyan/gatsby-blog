f9d0f7adce9d2ea132150cdcfbfc81955ddcb943
selimyan <gselimyan@gmail.com>
2022-07-01 15:57:56 -0400

    rendering data in blog template

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+
 import Layout from '../components/layout'
 
-const Blog = () => {
+export const query = graphql`
+  query ($slug: String!) {
+    markdownRemark (
+      fields: {
+        slug: {
+          eq: $slug
+        }
+      }
+    ) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const Blog = (props) => {
+  const { html, frontmatter: { title, date } } = props.data.markdownRemark
   return (
     <Layout>
-      Another Blog
+      <h1>{title}</h1>
+      <p>{date}</p>
+      <div dangerouslySetInnerHTML={{ __html: html }}></div>
     </Layout>
   )
 }
