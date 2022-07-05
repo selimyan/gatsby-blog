d12b63e3632cb7798126f02271d0a42cab2af54f
selimyan <gselimyan@gmail.com>
2022-07-05 16:19:31 -0400

    set up Contenful

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,14 @@ module.exports = {
     author: 'This shall remain unknown',
   },
   plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
+    'gatsby-plugin-image',
     'gatsby-plugin-sass',
     {
       resolve: 'gatsby-source-filesystem',
