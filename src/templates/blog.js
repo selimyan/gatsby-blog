1e04f1e6865d2057cade706be677f8c941c43f45
selimyan <gselimyan@gmail.com>
2022-07-07 21:15:13 -0400

    fetch blog post images from Contenful

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,33 +1,54 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 import Layout from '../components/layout'
 
 export const query = graphql`
   query ($slug: String!) {
-    markdownRemark (
-      fields: {
-        slug: {
-          eq: $slug
+    contentfulBlogPost (
+      slug: {
+        eq: $slug
+        }
+      ) {
+      title
+      publishedDate (
+        formatString: "MMMM Do, YYYY"
+      )
+      body {
+        raw
+        references {
+          gatsbyImageData
+          contentful_id
+          title
         }
       }
-    ) {
-      frontmatter {
-        title
-        date
-      }
-      html
     }
   }
 `
 
 const Blog = (props) => {
-  const { html, frontmatter: { title, date } } = props.data.markdownRemark
+  const { title, publishedDate, body } = props.data.contentfulBlogPost
+  const { raw, references } = body
+  const parsedBody = JSON.parse(raw)
+
+  const options = {
+    renderNode: {
+      "embedded-asset-block": () => {
+        const image = getImage(references[0].gatsbyImageData)
+        const alt = references[0].title
+
+        return <GatsbyImage alt={alt} image={image}/>
+      }
+    }
+  }
+
   return (
     <Layout>
       <h1>{title}</h1>
-      <p>{date}</p>
-      <div dangerouslySetInnerHTML={{ __html: html }}></div>
+      <p>{publishedDate}</p>
+      {documentToReactComponents(parsedBody, options)}
     </Layout>
   )
 }
